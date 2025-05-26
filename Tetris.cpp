757de1d988aa0715d9766cf9c73607c37b18ebd7
devcyh <dydgustm@naver.com>
2025-05-26 15:55:38 +0900

    Tetris.h 작성

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,22 +1,4 @@
-﻿#include <iostream>
-#include <cstdint>
-#include <ctime>
-//#include <mmsystem.h>
-
-extern "C" {
-#include "core/inc/blocks.h"
-#include "driver/inc/key_driver.h"
-#include "driver/inc/cell.h"
-#include "core/inc/board.h"
-#include "model.h"
-}
-
-#include "Game.h"
-
-#pragma comment(lib, "winmm.lib") // winmm 라이브러리 링크
-
-#define BOARD_START_X (4)
-#define BOARD_START_Y (4)
+﻿#include "Tetris.h"
 
 class Tetris : public IGame {
 private:
